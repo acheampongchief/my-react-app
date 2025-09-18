12681c47bc6fb17e92a8a7a0d990f2aafa3f126c
Chief Acheampong <acheampongchief0@gmail.com>
2025-09-18 11:27:49 +0000

    Created the foodNutrients components and updated the capturePhoto

--- src/App.js
+++ src/App.js
@@ -1,122 +1,12 @@
 import React from 'react';
-/*
-  writing a program to print numbers from 1 to 32 
-  using tailwind css colors to print colors in react app 
-  parity are described using the respective colors
-  red for prime
-  green for even
-  blue for odd
-*/
-const numbers = Array.from({length: 32}, (_, i) => i + 1);
+import CameraCapture from './capturePhoto.js';
 
-const isPrime = (numbers) => {
-  if (numbers < 2) {
-    return false;
-  }
-  else if (numbers === 2) {
-    return true;
-   }
-  else {
-    for (let i = 2; i <= Math.sqrt(numbers); i++) {
-      if (numbers % i === 0) {
-        return false;
-      } else {
-        return true;
-      }
-    }
-    
-  }
-}  
 
-const NumList = () => {
-  return(
-    <div className="p-4">
-      <h1 className="text-4xl font-bold mb-4 text-center">Number List with Colors</h1>
-      <div className="space-y-2 text-center">
-        {numbers.map((numbers) => {
-          let colorClass = '';
-          if (isPrime(numbers)) {
-            colorClass = 'bg-red-500'; // Red for prime numbers
-          } else if (numbers % 2 === 0) {
-            colorClass = 'bg-green-500'; // Green for even numbers
-          } else {
-            colorClass = 'bg-blue-500'; // Blue for odd numbers
-          }
-            return (
-            <React.Fragment key={numbers}>
-              <span className={`text-lg  font-medium inline-block border px-7 py-7 ${colorClass}`}>
-              {numbers}
-              </span>
-              {(numbers % 7 === 0) && <br />}
-            </React.Fragment>
-            );
-        })}
-      </div>
-    </div>
-  );
-}
-const Greetings = () => {
-  return (
-    <div>
-      <h1>Welcome to Calorie Counter.</h1>
-    </div>
-  )
-}
-const addFood = (props) => {
-  const Food = [];
-  const handleClick = () => {
-    
-  }
-  return (
-     <div>
-     <button onClick = {handleClick} className= "" >Add Food</button>
-     </div>
-
-)
-}
-const addFood = (props) => {
-  return (
-    <div>
-<button>Add Food</button>
-    </div>
-  )
-}
-const Calories = (props) => {
-  return(
-    <div>
-      <p className='border'>Calories</p>
-    </div>
-  )
-}
-const Carbs = (props) => {
-  return (
-    <div>
-      <h2>Carbs</h2>
-    </div>
-  )
-}
-const Protein = (props) => {
-  return (
-    <div>
-      <p>Protein</p>
-    </div>
-  )
-}
-const Fat = (props) => {
-  return (
-    <div>
-      <p>Fat</p>
-    </div>
-  )
-}
 const App = () => {
   return (
-    <div>
-  <NumList />
-  <h2 className='text-center text-orange-600 text-3xl font-extrabold'>HOORAY!! HOORAY!! HOORAY!!</h2>
-    </div>
+   <div>
+    <CameraCapture />
+   </div> 
   )
 }
 export default App;
-// Successfully written to code to check the parity with numbers
-// HOORAY!! HOORAY!! HOORAY!!
